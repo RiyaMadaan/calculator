652bd4810c7e939461e9805f7564c99631c3d944
Riya <riyamadaan404@gmail.com>
2020-04-25 22:58:36 +0530

    changes committed

--- src/methods.js
+++ src/methods.js
@@ -1,71 +1,82 @@
 const{
     log,
-    tab
+    tab,
+    clear
 } = require("./util.js");
+
+const{
+    welcomeScreen
+} = require("./welcome.js");
+
 
 var readlineSync = require("readline-sync");
 
 var chalk = require("chalk");
 
-
-
-const name = function(){
-var name = readlineSync.question(chalk.yellowBright("Enter your name here:-"));
-    console.log(chalk.yellowBright("   Hi " + name + "!"));
-}
+const start = function() {
+    clear();
+    welcomeScreen();
+    // name();
+    menu();
+  }
+  
 
 
 const menu = function(){
-    var option = readlineSync.question(chalk.cyanBright(tab+"These are the operations you can do:-\n\n\t\t\t1.  ADD(+)\t\t\t\t2.  SUBTRACT(-)\n\t\t\t3.  PRODUCT(*)\t\t\t\t4.  DIVIDE(/)\n\t\t\t5.  SQUARE(x^2)\t\t\t\t6.  CUBE(x^3)\n\t\t\t7.  PERCENTAGE(%)\t\t\t8.  Exit\n\nHit the numeric value as directed:-")); 
-    operations(option);    
+    var message = "Hit the numeric value as directed:- "
+    var option = readlineSync.question(chalk.cyanBright(tab+"\nThese are the operations you can do:-\n\n\t\t\t1.  ADD(+)\t\t\t\t2.  SUBTRACT(-)\n\t\t\t3.  PRODUCT(*)\t\t\t\t4.  DIVIDE(/)\n\t\t\t5.  SQUARE(x^2)\t\t\t\t6.  CUBE(x^3)\n\t\t\t7.  PERCENTAGE(%)\t\t\t0.  Exit\n\n" + message)); 
+    operations(option);  
+    return message;  
 }
 
 
-const operations = function(option){
+const operations = function(option, message){
       if(option==1){
           add();
-          menu();
-      }
+          setTimeout(start, 3000);
+        }
       else if(option==2){
           subtract();
-          menu();
+          setTimeout(start, 3000);
       }
       else if(option==3){
           multiply();
-          menu();
+          setTimeout(start, 3000);
       }
       else if(option==4){
           divide();
-          menu();
+          setTimeout(start, 3000);
       }
       else if(option==5){
         square();
-        menu();
+        setTimeout(start, 3000);
     }
     else if(option==6){
         cube();
-        menu();
+        setTimeout(start, 3000);
     }
       else if(option==7){
          percentage();  
-         menu();
+         setTimeout(start, 3000);
       }
-      else if(option==8){
-          log(chalk.blueBright(tab+ "Thankyou for using this~~~~~~~~~~~~Hope your experience was best"));
+      else if(option==0){
+          log(chalk.yellowBright(tab+ "Thankyou for using this~~~~~~~~~~~~Hope your experience was best!!......"));
           return;
       }
       else{
           log(chalk.red("ERROR:  Please enter valid choice"));
-          menu();
+          log(chalk.white("Try again"));
+        setTimeout(start,2000);   
+       
       }
 }
- 
 
 const add = function(){
     var number1 = readlineSync.question(chalk.yellowBright("Enter first number: "));
     var number2 = readlineSync.question(chalk.yellowBright("Enter second number: "));
    var sum = (+number1) + (+number2);
-    log(number1 + " + " + number2 + " is "  + sum);
+    log(number1 + " + " + number2 + " => "  + sum);
+    
     }
 
 
@@ -74,7 +85,8 @@ const subtract = function(){
     var number1 = readlineSync.question(chalk.yellowBright("Enter first number: "));
     var number2 = readlineSync.question(chalk.yellowBright("Enter second number: "));
    var sub = number1 - number2;
-    log(number1 + " - " + number2 + " is "  + sub);
+  log(number1 + " - " + number2 + " => "  + sub);
+    
 }
 
 
@@ -82,7 +94,8 @@ const multiply = function(){
     var number1 = readlineSync.question(chalk.yellowBright("enter first number: "));
     var number2 = readlineSync.question(chalk.yellowBright("enter second number: "));
    var product = number1 * number2;
-    log(number1 + " * " + number2 + " is " + product);
+    log(number1 + " * " + number2 + " => " + product);
+    
 }
 
 
@@ -90,7 +103,8 @@ const divide = function(){
     var number1 = readlineSync.question(chalk.yellowBright("Enter first number: "));
     var number2 = readlineSync.question(chalk.yellowBright("Enter second number: "));
    var div = number1 / number2;
-    log(number1 + " / " + number2 + " is " + div);
+    log(number1 + " / " + number2 + " => " + div);
+
 }
 
 
@@ -98,24 +112,27 @@ const percentage = function(){
     var number = readlineSync.question(chalk.yellowBright("Enter the number: "));
     var howMuchPercent = readlineSync.question(chalk.yellowBright("Enter the percent: "));
     var  percent = (number*howMuchPercent)/100;
-    log(howMuchPercent +"%" + " of " + number + " is " + percent);
+    log(howMuchPercent +"%" + " of " + number + " => " + percent);
+
 }
 
 const square = function(){
     var number = readlineSync.question(chalk.yellowBright("Enter the number: "));
     var squaredNumber = number * number;
-    log("Square of " + number + " is " + squaredNumber);
+    log("Square of " + number + " => " + squaredNumber);
+
 }
 
 const cube = function(){
     var number = readlineSync.question(chalk.yellowBright("Enter the number: "));
     var cubed = number * number * number;
-    log("Cube of " + number + " is " + cubed);
+    log("Cube of " + number + " => " + cubed);
+
 }
 
 module.exports = {
-    name,
     menu,
     percentage,
-    operations
+    operations,
+    start
 }
--- src/util.js
+++ src/util.js
@@ -1,7 +1,10 @@
 var log = console.log;
-const tab = "\t\t\t\t";
-
+const tab = "\t\t\t\t\t";
+const clear = console.clear;
+const chalk = require('chalk');
 module.exports = {
     log,
-    tab
+    tab,
+    clear,
+    chalk
 }
